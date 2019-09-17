6795224638e9ac264a8c73b81aa3436e953be6ca
sanky2400 <s1r2s3@gmail.com>
2019-09-17 15:05:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+#Title - Variables for my favourite song
+import datetime
+#Song - Yellow
+
+#Variables
+songName = "Yellow"  #Name of the song
+band = "ColdPlay"    #Song Played by the band
+singer = "Chris Martin"     #Lead Singer
+guitar = ["Jhonny Buckland", "Guy Berryman"]    #Lead Guitarists
+drums = "Will Champion"
+genre = "Alternative Rock"  #Category of music
+albumName= "Parachutes"     #Name of the album
+Label = "Parlophone"        #Company where song was recorded
+songLengthInSeconds = 271   #Length of song in seconds
+songLengthInMinutes = "04:31"   #Length of song in minutes
+bandSize = 5          #Size of band
+releaseDate = datetime.date(2000,6,26)      #Relase date of the song
+recordedYear = 2000          #Year reacorded
+recordedMonth = "March"
+songWriters = ["Chrish Martin","Jhonny Buckland","Guy Berryman","Will Champion"]
+producer = ["Ken Nelson", "Coldplay"]       #Song produced by
+nominatedForAwards = ["Best Single","British single of the year", "British video of the year", "International Hit of the year", "Best Rock Performance by a Duo or Group with Vocal", "Best Rock Song","Best New Artist"]       #Number of nominations this song got
+awardsWon = "Best Single"       #Number of awards this osng won
+numberOfTimesNominated = 7      #number of times it got nominated
+numberOfTimeWon = 1             #Number of times it won
+youCanFindItOnline = True
+websiteWhereYouCanFindThisSong = ["www.gaana.com","www.youtube.com","www.jiosaavn.com"]     #Websites where one can find this song
+mobileApplicationsWhereYouCanFindIt = ["Gaana","Spotify","Amazon Music","Youtube","JioSaavn"]       #Mobile application where one can find this song
+urlForTheSong = ["https://www.gaana.com/search/yellow","https://www.youtube.com/watch?v=yKNxeF4KMsY"]   #Direct url's to the song
+
+print("Name of the song:\n"+songName+"\n")
+print("Name of the band:\n"+band+"\n")
+print("Name of the singer:\n"+singer+"\n")
+print("Below are the names of the guitarists:")
+print(guitar)
+print("\n")
+print("Name of the drummer:\n"+drums+"\n")
+print("Genre of the song is:\n"+genre+"\n")
+print("Name of the album is:\n"+albumName+"\n")
+print("Name of the label is:\n"+Label+"\n")
+print("Length of the song in seconeds:")
+print(songLengthInSeconds)
+print("\n")
+print("Length of the song in minutes:\n"+songLengthInMinutes+"\n")
+print("Size of the band: ")
+print(bandSize)
+print("\n")
+print("Release date of the song:")
+print(releaseDate)
+print("\n")
+print("The song was recorded in the year:")
+print(recordedYear)
+print("\n")
+print("The song was recorded in the month:\n"+recordedMonth+"\n")
+print("Writers of the song are: ")
+print(songWriters)
+print("\n")
+print("Name of the producer's:")
+print(producer)
+print("\n")
+print("Nominated for:")
+print(nominatedForAwards)
+print("\n")
+print("Awards won:\n"+awardsWon+"\n")
+print("Number of time's this song was nominated:")
+print(numberOfTimesNominated)
+print("\n")
+print("Number of time's this song won the award:")
+print(numberOfTimeWon)
+print("\n")
+print("Can you find this song online:")
+print(youCanFindItOnline)
+print("\n")
+print("Websites where you can find this song:")
+print(websiteWhereYouCanFindThisSong)
+print("\n")
+print("Monile applications where you can find this song:")
+print(mobileApplicationsWhereYouCanFindIt)
+print("\n")
+print("Direct link to the song")
+print(urlForTheSong)
+print("\n")
